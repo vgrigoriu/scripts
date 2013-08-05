96c2f1a9be127c74dcaab82c2f43f88234ece8df
Victor Grigoriu <vgrigoriu@gmail.com>
2013-08-05 13:43:39 +0300

    Move chocolatey scripts to the scripts repo

---
+++ DevMachineInstall/devApps.ps1
@@ -0,0 +1,52 @@
+# Dev apps to install
+
+# media stuff
+cinstm vlc
+cinstm calibre
+cinstm foobar2000
+cinstm mp3tag
+cinstm iTunes
+
+cinstm SublimeText3.app
+cinstm githubforwindows
+
+cinstm PowerShell
+
+cinstm windirstat
+cinstm lockhunter
+
+cinstm FoxitReader
+cinstm lastpass
+
+# Visual studio stuff
+#cinstm VisualStudio2013Ultimate
+#cinstm VisualStudio2012Ultimate
+
+# Visual studio add-ins
+#cinstm xUnit.vs2012
+#cinstm WebEssentials2012
+#cinstm resharper -Version 7.1.3000.2254 # Use only resharper if you want the latests. This is what I have a license for
+#cinstm typescript
+#cinstm typescript.vs
+#cinstm SpecFlow
+
+# databases and tools
+#cinstm MsSqlServer2012Express
+#cinstm mongodb
+#cinstm MongoVUE
+
+# browsers
+#cinstm Firefox
+#cinstm GoogleChrome
+
+# Other tools
+#cinstm git
+#cinstm notepadplusplus
+cinstm winmerge
+#cinstm Console2
+cinstm 7zip
+#cinstm nodejs.install
+cinstm fiddler
+#cinstm curl
+#cinstm linqpad4
+
---
+++ DevMachineInstall/devInstall.bat
@@ -0,0 +1,6 @@
+# INSTALL CHOCOLATERY
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+
+
+# INSTALL MARCUS DEV APPS 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://github.com/vgrigoriu/scripts/raw/master/DevMachineInstall/devApps.ps1'))" 
