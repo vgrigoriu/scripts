5ec5f4162c13b929c366e3caab9308706a4cecdd
Victor Grigoriu <vgrigoriu@gmail.com>
2013-08-05 13:59:12 +0300

    Correct comment

--- DevMachineInstall/devInstall.bat
+++ DevMachineInstall/devInstall.bat
@@ -2,5 +2,5 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
 
 
-# INSTALL MARCUS DEV APPS 
+# INSTALL APPS 
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://github.com/vgrigoriu/scripts/raw/master/DevMachineInstall/devApps.ps1'))" 
