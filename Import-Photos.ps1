0de7dfbaab97a09705ee16f9278b34b8fb0a42d9
Victor Grigoriu <vgrigoriu@gmail.com>
2013-08-19 23:21:04 +0300

    Actually use the argument in Import-Pictures
    Plus changed the destination to F:

--- Import-Photos.ps1
+++ Import-Photos.ps1
@@ -1,12 +1,12 @@
 param($location = "")
 
-$pattern = "D:\pozetest\%Y-%m"
+$pattern = "f:\pze\%Y-%m-%d"
 
 if ($location -ne "") {
 	$pattern = $pattern + " $location"
 }
 
 # asta merge
-.\bin\exiftool.exe -directory<datetimeoriginal D:\poze\Provence\DSC_3159.JPG -d d:\pze\%Y-%m -o cucu\
+.\bin\exiftool.exe -directory<datetimeoriginal D:\DCIM\100NCD70 -d $pattern -o cucu\
 
 #.\bin\exiftool -o -d $pattern "-directory<datetimeoriginal" D:\poze\Provence
