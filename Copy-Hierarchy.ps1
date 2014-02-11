886adb0f66221b990b5e77fdf54df1117e058751
Victor Grigoriu <vgrigoriu@gmail.com>
2014-02-11 22:51:22 +0200

    first version of copy-hierarchy

---
+++ Copy-Hierarchy.ps1
@@ -0,0 +1,18 @@
+Param([string]$Source, [string]$Destination, [string[]]$Extensions)
+
+Push-Location $Source
+
+Get-ChildItem . -Recurse |
+Where-Object {
+    $extensions -contains $_.Extension
+} |
+ForEach-Object {
+    $sourcePath = Resolve-Path $_.FullName -relative
+    $destPath = Join-Path $Destination (Split-Path $sourcePath)
+    if (-not (Test-Path $destPath)) {
+        New-Item $destPath -ItemType directory
+    }
+    Copy-Item $sourcePath $destPath
+}
+
+Pop-Location
